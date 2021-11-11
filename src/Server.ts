3162078f33377c20b7856049d6263b42edeb5918
Aziiiz <asirojiddinov96@gmail.com>
2021-11-11 16:18:32 +0900

    cors

--- src/Server.ts
+++ src/Server.ts
@@ -39,9 +39,25 @@ app.use('/api', BaseRouter);
 // Print API errors
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    return res.status(BAD_REQUEST).json({
-        error: err.message,
-    });
+    	// Website you wish to allow to connect
+		res.setHeader('Access-Control-Allow-Origin', '*');
+
+		// Request methods you wish to allow
+		res.setHeader(
+			'Access-Control-Allow-Methods',
+			'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+		);
+
+		// Request headers you wish to allow
+		res.setHeader(
+			'Access-Control-Allow-Headers',
+			'X-Requested-With,content-type'
+		);
+
+		// Set to true if you need the website to include cookies in the requests sent
+		// to the API (e.g. in case you use sessions)
+		res.setHeader('Access-Control-Allow-Credentials', 'true');
+		next();
 });
 
 
