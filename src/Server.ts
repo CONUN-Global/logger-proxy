7ccb44637ba109bf0cc8d347744ea417203c8460
Aziiiz <asirojiddinov96@gmail.com>
2021-11-11 16:27:52 +0900

    cors

--- src/Server.ts
+++ src/Server.ts
@@ -21,7 +21,27 @@ const { BAD_REQUEST } = StatusCodes;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use((req:Request, res: Response, next: NextFunction) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
+    // Request methods you wish to allow
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-Requested-With,content-type'
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 // Show routes called in console during development
 // if (process.env.NODE_ENV === 'development') {
 //     app.use(morgan('dev'));
@@ -34,30 +54,15 @@ if (process.env.NODE_ENV === 'production') {
 
 // Add APIs
 const apirouter = express.Router();
+
 app.use('/api', BaseRouter);
 
 // Print API errors
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    	// Website you wish to allow to connect
-		res.setHeader('Access-Control-Allow-Origin', '*');
-
-		// Request methods you wish to allow
-		res.setHeader(
-			'Access-Control-Allow-Methods',
-			'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-		);
-
-		// Request headers you wish to allow
-		res.setHeader(
-			'Access-Control-Allow-Headers',
-			'X-Requested-With,content-type'
-		);
-
-		// Set to true if you need the website to include cookies in the requests sent
-		// to the API (e.g. in case you use sessions)
-		res.setHeader('Access-Control-Allow-Credentials', 'true');
-		next();
+    return res.status(BAD_REQUEST).json({
+        error: err.message,
+    });
 });
 
 
